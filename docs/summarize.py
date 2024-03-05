6804d39fc763d8c9352ebf14a5938ce7814e9481
Anton Shpakovych <shpakovych18@gmail.com>
2024-03-05 20:30:55 +0200

    Modified description for endpoint, added .env.sample for reviewer

--- docs/summarize.py
+++ docs/summarize.py
@@ -13,6 +13,6 @@ OUTPUT_EXAMPLE = (
     "Its base is square, measuring 125 metres (410 ft) on each side. During its construction, "
     "the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world."
 )
-DESCRIPTION = "Provide summary for your text"
+DESCRIPTION = "Provide summary for your text [ONLY ENGLISH]."
 RATE_LIMITER_TIMES = 1
 RATE_LIMITER_SECONDS = 30
